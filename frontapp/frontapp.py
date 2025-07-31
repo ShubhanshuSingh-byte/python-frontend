0318b5aca37bc44757b5d1b5dc1b33c581d80ff4
ShubhanshuSingh-byte <sshubhanshu0019@gmail.com>
2025-07-31 07:28:48 +0530

    Add new folder: frontapp

---
+++ frontapp/frontapp.py
@@ -0,0 +1,30 @@
+import reflex as rx
+from rxconfig import config
+
+
+class State(rx.State):
+    """The app state."""
+
+
+def login_page():
+    return rx.flex(
+        rx.box(
+            rx.vstack(
+                rx.image(src="logo.png", alt="Logo"),
+                rx.text("Welcome to SNSS Global Services", weight="bold", size="9", align="center"),
+                rx.text("Enterprise Resource Planning System"),
+                rx.button(rx.image(src="logo.png", width="20px", height="20px"), rx.text("Continue with google", padding = "3em")),
+                rx.text("Application under development", padding = "3em"),
+                align="center"
+            ),
+            width="450px", height="539px", padding="50px"
+        ),
+        justify="center"
+    )
+
+
+def index():
+    return login_page()
+
+app = rx.App()
+app.add_page(index)
